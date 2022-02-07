6d4fea58303c2a87f16372494d4d1bf7e234bac1
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-02-06 23:28:42 -0300

    wrote models and migration files

--- phonebook/db/migrate/20220204152045_create_phone_numbers.rb
+++ phonebook/db/migrate/20220204152045_create_phone_numbers.rb
@@ -1,12 +1,13 @@
 class CreatePhoneNumbers < ActiveRecord::Migration[6.1]
   def change
     create_table :phone_numbers do |t|
-      t.string :number
+      t.string :number, null: false
       t.string :num_type
-      t.boolean :main_number
+      t.boolean :main_number, null: false
       t.belongs_to :contact
-      t.references :contact
+      t.references :contact, foreign_key: true
 
+      t.index :number, unique: true
       t.timestamps
     end
   end
