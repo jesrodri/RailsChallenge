739a887794e2d1b44bd33273ce826e01760c5e8f
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-02-07 14:40:16 -0300

    first spec files

--- phonebook/app/models/user.rb
+++ phonebook/app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :contacts
-  has_many :phone_numbers, through :contacts
 
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true, uniqueness: true
 end
